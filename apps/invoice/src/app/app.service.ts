86ef8208426bce0fe6118ce2956de2506f0df7a8
Dinh Van <todinhvan2608@gmail.com>
2025-10-07 23:20:52 +0700

    Impl shared library

--- apps/invoice/src/app/app.service.ts
+++ apps/invoice/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PORT } from '@common/constants/common.constant';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello API' };
+    const port = PORT;
+    return { message: 'Hello API' + port };
   }
 }
