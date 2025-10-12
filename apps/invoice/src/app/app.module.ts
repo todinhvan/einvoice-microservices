edec980062973aa518e38d4795b79a3347a8832c
To Dinh Van <todinhvan2683@gmail.com>
2025-10-12 23:37:30 +0700

    chore/#: Setup mongodb and fix decorators folder outside libs

--- apps/invoice/src/app/app.module.ts
+++ apps/invoice/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CONFIGURATION, TConfiguration } from '../configuration';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       load: [() => CONFIGURATION],
     }),
+    MongooseModule.forRoot('mongodb://root:password@localhost:27017/'),
   ],
   controllers: [AppController],
   providers: [AppService],
